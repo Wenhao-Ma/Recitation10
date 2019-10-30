932abdd69faaa79d1ca8c2fc8da4484efdb27044
wenhaom <wenhaom@andrew.cmu.edu>
2019-10-30 13:14:58 -0400

    Fix wrong package name

--- src/main/java/edu/cmu/cs/cs214/rec10/plugin/MoneyDoorPlugin.java
+++ src/main/java/edu/cmu/cs/cs214/rec10/plugin/MoneyDoorPlugin.java
@@ -1,4 +1,4 @@
-package main.java.edu.cmu.cs.cs214.rec10.plugin;
+package edu.cmu.cs.cs214.rec10.plugin;
 
 import edu.cmu.cs.cs214.rec10.framework.core.GameFramework;
 import edu.cmu.cs.cs214.rec10.framework.core.GamePlugin;
